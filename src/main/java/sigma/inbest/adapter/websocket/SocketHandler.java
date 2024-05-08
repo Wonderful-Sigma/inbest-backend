aa0e7e662da6958d0ccfd56d45739859e09c51c9
BeomJun Jang <wkdqjawns@dgsw.hs.kr>
2024-05-08 11:37:14 +0900

    chore: test 코드 추가, socket 설정 일부 추가

---
+++ src/main/java/sigma/inbest/adapter/websocket/SocketHandler.java
@@ -0,0 +1,22 @@
+package sigma.inbest.adapter.websocket;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SocketHandler extends TextWebSocketHandler {
+    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
